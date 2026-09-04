5c450279d0cd319f92da3cd2e26cb37fef36722c
PRINCE SINGH <singhprince8118@gmail.com>
2026-09-04 13:13:37 +0530

    Update app.py

--- Heart_health/app.py
+++ Heart_health/app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import os
 
-model = joblib.load("KNN_heart.pkl")
-scaler = joblib.load("scaler.pkl")
-expected_columns = joblib.load("columns.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+model = joblib.load(os.path.join(BASE_DIR, "KNN_heart.pkl"))
+scaler = joblib.load(os.path.join(BASE_DIR, "scaler.pkl"))
+expected_columns = joblib.load(os.path.join(BASE_DIR, "columns.pkl"))
 
 st.title("Heart Stroke Prediction By Prince ❤️")
 st.markdown("Provide the following details")
